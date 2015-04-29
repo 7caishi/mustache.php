6afb230de67068da2b4d00670660d4a2016abdc5
zip <ziyang.liu@yoho.cn>
2015-04-29 20:52:35 +0800

    fix MustacheException

---
+++ src/Mustache/Exception/RuntimeException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zip
+ * Date: 15/4/29
+ * Time: 下午8:51
+ */
+
+namespace Mustache\Exception;
+
+use Mustache\MustacheException;
+
+class RuntimeException extends \RuntimeException implements MustacheException
+{
+
+} 
---
+++ src/Mustache/MustacheException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zip
+ * Date: 15/4/29
+ * Time: 下午8:50
+ */
+
+namespace Mustache;
+
+
+interface MustacheException {
+
+} 
